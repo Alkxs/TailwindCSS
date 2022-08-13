7815e3216263a395a419a9cd6e28927833377d35
Alkxs <72358387+Alkxs@users.noreply.github.com>
2022-08-13 23:49:06 +0200

    first commit final project

--- Final project/tailwind.config.js
+++ Final project/tailwind.config.js
@@ -1,8 +1,22 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    // colors: {
+    //   'blue': '#1fb6ff',
+    //   'purple': '#7e5bef'
+    // },
+    // fontFamily {
+    //   sans: ['Graphik', 'sans-serif'],
+    //   serif: ['Merriweather', 'serif']
+  },
+  extend: {
+    //   spacing: {
+    //     '8xl': '96rem',
+    //     '9xl': '128rem',
+    //   },
+    //   borderRadius: {
+    //     '4xl': '2rem',
+    //   }
   },
   plugins: [],
 };
