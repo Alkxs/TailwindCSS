701c14eec87d67a409a2ab3bd8533be41e71a29a
Alkxs <72358387+Alkxs@users.noreply.github.com>
2022-09-04 17:39:35 +0200

    section download completed

--- Final project/js/script.js
+++ Final project/js/script.js
@@ -1,3 +1,4 @@
+////////////////light-dark mode////////////////////
 const themeToggleBtn = document.getElementById('theme-toggle');
 const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
 const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
@@ -37,4 +38,23 @@ function toggleMode() {
       localStorage.setItem('color-theme', 'dark');
     }
   }
+}
+/////////////////Features toggle ///////////////////
+const tabs = document.querySelectorAll('.tab');
+const panels = document.querySelectorAll('.panel');
+
+// Tabs menu event listener
+tabs.forEach((tab) => tab.addEventListener('click', onTabClick));
+
+function onTabClick(e) {
+  // Deactivate all tabs
+  tabs.forEach((tab) => {
+    tab.children[0].classList.remove('border-[#891112]', 'border-b-4', 'md:border-b-0');
+  });
+  // Hide all panels
+  panels.forEach((panel) => panel.classList.add('hidden'));
+  // Activate a new tab and panel based on the target
+  e.target.classList.add('border-[#891112]', 'border-b-4');
+  const classString = e.target.getAttribute('data-target');
+  document.getElementById('panels').getElementsByClassName(classString)[0].classList.remove('hidden');
 }
